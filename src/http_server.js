1f9f8514a9f0261488b37024c5c049ac60193d25
ckhomo <ck9030311@gmail.com>
2020-03-18 16:06:46 +0800

    0318

--- src/http_server.js
+++ src/http_server.js
@@ -2,10 +2,12 @@
 const http = require('http');
 const server = http.createServer((request, response) => {
     response.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type': 'text/html; charset="utf-8"'
     });
-    response.end(`<div>???Hello???<br>
+    response.end(`<div>Hello, YU_LI here.</div>
+                  <div>こんにちは。こちらはYU_LIです。</div>
+                  <div>Здравствуй, здесь YU_LI.</div><br>
 ${request.url}
 </div>`);
 });
-server.listen(3000);
+server.listen(9487);
