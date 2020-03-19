d3c7cd7d89142c6f85a73f6bf79e3ba9912c4d0a
ckhomo <ck9030311@gmail.com>
2020-03-19 12:03:26 +0800

    0319

--- src/index.js
+++ src/index.js
@@ -2,23 +2,35 @@ const express = require('express');
 
 var app = express();
 
+// Setup view engine:
+app.set('view engine', 'ejs');
+
 //順序:由上而下。
 //若有路徑衝突，會優先send上層之連結。
 
 // Link to designated pages.
-app.get('/',(req, res)=>{
-    res.send(`<h2>Hello Mr.Lee!</h2>`);
+app.get('/', (req, res) => {
+    // res.send(`<h2>Hello Mr.Lee!</h2>`);
+    res.render('home', { name: "ckhomo" });
 });
 
-app.get('/try',(req,res)=>{
+app.get('/try', (req, res) => {
     res.send(`<h2>TRY THIS ROUTER.</h2>`);
+});
+
+app.get('/sales', (req, res) => {
+    const data = require(__dirname + '/../data/sales');
+    // res.send(`<h2>${data[0].name}</h2>`);
+    res.render('sales', {
+        sales: data
+    });
 });
 
 // Static folder:
 app.use(express.static("public"));
 
 // Post error message.
-app.use((req,res)=>{
+app.use((req, res) => {
     res.type('text/html');
     res.status = 404;
     res.send(`
@@ -27,6 +39,6 @@ app.use((req,res)=>{
     `);
 });
 
-app.listen(5487, function(){
-    console.log("Listening on port:5487.");  
+app.listen(5487, function () {
+    console.log("Listening on port:5487.");
 });
